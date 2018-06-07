ed48639589648c2d9ef5a24aeb11182e2c306666
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-06-07 16:48:15 -0400

    jest is finally configured

--- testing/react/jest.config.js
+++ testing/react/jest.config.js
@@ -1,20 +1,20 @@
 const {defaults} = require('jest-config');
 
 module.exports = {
-  moduleDirectories: ["node_modules", "."],
+  moduleDirectories: ["node_modules", ".", "src", "tests"],
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
   setupTestFrameworkScriptFile: '<rootDir>/tests/support/setup.js',
   testMatch: [
      "**/__tests__/**/*test.ts",
-     "**/*test.ts"
+     "**/*test.ts",
+     "**/*test.js"
   ],
   transform: {
-   "^.+\\.(ts|tsx)$": "ts-jest"
+   "^.+\\.(t|j)sx?$": "ts-jest"
   },
   globals: {
     "ts-jest": {
-      tsConfigFile: "tsconfig.jest.json",
-      skipBabel: true
+      tsConfigFile: "tsconfig.jest.json"
     }
   },
 }
---
+++ testing/react/tests/acceptance/todo-editing-test.ts
@@ -0,0 +1,15 @@
+import puppeteer from 'puppeteer';
+
+describe('Acceptance | todo editing', () => {
+    test('the initial todo can be edited', () => {
+
+    });
+
+    test('the initial todo can have the text change', () => {
+
+    });
+
+    test('the initial todo can be completed', () => {
+
+    });
+});
