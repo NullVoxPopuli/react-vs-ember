677cfbbd73528242f22e4c6d639b44c3fbdddcbf
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-06-09 07:16:58 -0400

    well... I think async is breaking the detection of tests

--- testing/react/tests/cypress/integration/TodoMVC/list-filtering.spec.ts
+++ testing/react/tests/cypress/integration/TodoMVC/list-filtering.spec.ts
@@ -1,19 +1,25 @@
+/// <reference types="Cypress" />
+
 import { toggle, add } from "example-app/redux-store/todos";
 
 const host = 'localhost:1234';
 
-context('Querying', () => {
+context('Acceptance | list filtering', () => {
   const find = selector => cy.get(selector);
-  beforeEach(() => {
-    cy.visit(host);
-    cy.window().its('__store__').then(store => {
-      store.dispatch(add('beforeEach todo 1'));
-      store.dispatch(add('beforeEach todo 2'));
-      store.dispatch(toggle(1)); // complet todo with id 1
-    });
+
+  beforeEach(async () => {
+    await cy.visit(host);
+
+    const store = await cy.window().its('__store__');
+
+    store.dispatch(add('beforeEach todo 1'));
+    store.dispatch(add('beforeEach todo 2'));
+    store.dispatch(toggle(1)); // complet todo with id 1
   });
 
   it('lists all todos', async done => {
+    await find('.filters a').first().click();
+
     const elements = await find('ul.todo-list li');
     const length = elements.length;
 
@@ -21,11 +27,23 @@ context('Querying', () => {
     done();
   });
 
-  it('lists only completed todos', done => {
-    // cy.
+  it('lists only completed todos', async done => {
+    await find('.filters a').last().click();
+
+    const elements = await find('ul.todo-list li');
+    const length = elements.length;
+
+    expect(length).to.eq(2);
+    done();
   });
 
-  it('lists only active todos', done => {
+  it('lists only active todos', async done => {
+    await find('.filters a').click();
 
-  })
+    const elements = await find('ul.todo-list li');
+    const length = elements.length;
+
+    expect(length).to.eq(2);
+    done();
+  });
 });
--- testing/react/tests/cypress/integration/TodoMVC/todo-editing.spec.ts
+++ testing/react/tests/cypress/integration/TodoMVC/todo-editing.spec.ts
@@ -1,15 +1,41 @@
 /// <reference types="Cypress" />
+const host = 'localhost:1234';
+
 
 describe('Acceptance | todo editing', () => {
-    it('the initial todo can be edited', () => {
+  beforeEach(async () => {
+    await cy.visit(host);
+  });
 
-    });
+  const find = (selector: string): Cypress.Chainable<JQuery<any>> => cy.get(selector);
 
-    it('the initial todo can have the text change', () => {
+  const selectors = {
+    completed: 'ul.todo-list li.completed',
+    todos: 'ul.todo-list li',
+    todoInput: 'ul.todo-list li input.edit',
+    todoLabels: 'ul.todo-list li label',
+    completeCheckbox: (id: string) => `div[data-test-todo-id='${id}'] input.toggle`
+  };
 
-    });
+  const findAllTodos = () => find(selectors.todos);
+  const findFirstTodo = () => find(selectors.todos)[0];
+  const firstTodoText = () => findFirstTodo().textContent as string;
+  const findFirstLabel = () => find(selectors.todoLabels)[0];
 
-    it('the initial todo can be completed', () => {
 
-    });
+  it('the initial todo can be edited', async () => {
+    await findFirstLabel.click();
+
+    const todo = await findFirstTodo();
+
+    expect(todo.classList).to.contain('editing');
+  });
+
+  it('the initial todo can have the text change', () => {
+
+  });
+
+  it('the initial todo can be completed', () => {
+
+  });
 });
