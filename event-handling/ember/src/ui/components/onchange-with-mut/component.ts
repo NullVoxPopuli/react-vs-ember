83fca4899cac055d7f2998f7d0bf1d732ecf9a82
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-06-19 23:11:44 -0400

    demonstrate two-way binding

--- event-handling/ember/src/ui/components/onchange-with-mut/component.ts
+++
@@ -1,5 +0,0 @@
-import Component from '@ember/component';
-
-export default class MyComponent extends Component {
-  textProperty?: string;
-}
---
+++ event-handling/ember/src/ui/components/two-way-binding/component.ts
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import { action } from '@ember-decorators/object';
+
+export default class TwoWayBinding extends Component {
+  textProperty?: string;
+}
