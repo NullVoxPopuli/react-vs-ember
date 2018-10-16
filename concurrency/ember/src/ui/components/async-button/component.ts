787a339ce202523201eddd8c79dd7e5109bd562b
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-10-16 06:42:14 -0400

    yay ember concurrency demo

--- concurrency/ember/src/ui/components/async-button/component.ts
+++ concurrency/ember/src/ui/components/async-button/component.ts
@@ -1,12 +1,18 @@
 import Component from '@ember/component';
-import { task, timeout } from 'ember-concurrency';
+import { reads } from '@ember-decorators/object/computed';
+import { timeout } from 'ember-concurrency';
+import { task } from 'ember-concurrency-decorators';
 
 export default class AsyncButton extends Component {
   clickCount = 0;
 
-  didClick = task(function * () {
+  @reads('clickTask.lastSuccessful.value') lastCoords!: string;
+
+  @task * clickTask(this: AsyncButton, e: MouseEvent) {
     yield timeout(2000);
 
     this.set('clickCount', this.clickCount + 1);
-  });
+
+    return `${e.x} x ${e.y}`;
+  };
 }
