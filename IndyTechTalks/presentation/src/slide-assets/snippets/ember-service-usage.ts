50c338d68642a1bb75c7268fe4961f2973ea6bac
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-10-20 23:04:56 -0400

    some cleanup

--- IndyTechTalks/presentation/src/slide-assets/snippets/ember-service-usage.ts
+++ IndyTechTalks/presentation/src/slide-assets/snippets/ember-service-usage.ts
@@ -6,7 +6,7 @@ export default class extends Component {
   @service('todos') todoManager!: TodosService;
   @service router;
 
-  @equal('router.currentRouteName.routeName', 'index') showAll!: boolean;
+  @equal('router.currentRouteName', 'index') showAll!: boolean;
 
   @alias('todos.length') todosCount!: number;
   @gt('todosCount', 0) showClearButton!: boolean;
