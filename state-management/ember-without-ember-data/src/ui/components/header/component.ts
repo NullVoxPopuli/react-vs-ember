b1f8ae50d85fea9323d7424c942181b77c2bde92
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-03-10 22:36:55 -0400

    improvements

--- state-management/ember-without-ember-data/src/ui/components/header/component.ts
+++ state-management/ember-without-ember-data/src/ui/components/header/component.ts
@@ -1,15 +1,16 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 import TodosService from 'example-app/services/todos';
 
 export default class Header extends Component {
   @service todos!: TodosService;
 
-  text = '';
+  @tracked text = '';
 
   didSubmit(this: Header) {
     this.todos.add(this.text);
-    this.set('text', '');
+    this.text = '';
   }
 }
