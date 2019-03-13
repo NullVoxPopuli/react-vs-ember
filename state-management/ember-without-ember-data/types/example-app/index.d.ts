341ee8e53697a14e5710d1173dfb47f54448f0f1
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-03-13 19:24:30 -0400

    eh

--- state-management/ember-without-ember-data/types/example-app/index.d.ts
+++ state-management/ember-without-ember-data/types/example-app/index.d.ts
@@ -2,12 +2,9 @@
 import Ember from 'ember';
 
 declare global {
-  interface Window {
-    devToolsExtension?: () => void;
-  }
-
   interface Array<T> extends Ember.ArrayPrototypeExtensions<T> {}
   // interface Function extends Ember.FunctionPrototypeExtensions {}
 }
 
 export {};
+
